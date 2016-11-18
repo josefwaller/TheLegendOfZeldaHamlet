c90b77d097ff91d2209da8a866adf8a2ad831ccb
josefwaller <josef@siriusapplications.com>
2016-11-17 18:13:02 -0800

    Added App class

---
+++ the_legend_of_zelda_hamlet/src/App.java
@@ -0,0 +1,29 @@
+package src;
+
+// Superclass for Graphical use
+import java.awt.Frame;
+
+/*
+The main App class.
+This is the main class which runs the program
+*/
+class App {
+
+	/*
+	Returns a new App
+	Currently does nothing really
+	*/
+	public App() {
+
+	}
+
+	/*
+	Starts the game
+	*/
+	public static void main(String[] args) {
+
+		App app = new App();
+
+	}
+
+}
--- the_legend_of_zelda_hamlet/src/TheLegendOfZeldaHamlet.java
+++
@@ -1,8 +0,0 @@
-class TheLegendOfZeldaHamlet {
-	
-	public static void main (String[] args) {
-		
-		System.out.println("Hello World!");
-		
-	}
-}

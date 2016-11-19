a537c9331a983f1791506abfdc14a81fb596b01d
josefwaller <josef@siriusapplications.com>
2016-11-18 22:11:37 -0800

    Switched from AWT/Swing to Slick2D and lwjgl

---
+++ the_legend_of_zelda_hamlet/src/Game.java
@@ -0,0 +1,100 @@
+// The Graphics module
+// Uses Slick2D
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
+import org.newdawn.slick.Image;
+
+
+// The player Entity
+import entities.Player;
+
+/*
+The main Game class.
+This is the main class which runs the program
+*/
+public class Game extends BasicGame {
+
+	// The dimensions of the window
+	private int w;
+	private int h;
+	
+	// the player entity
+	private Player player;
+
+	/*
+	Returns a new Game
+	Sets up window and game
+	*/
+	public Game(int w, int h) {
+
+		// sets the title
+		super("The Legend of Zelda: Hamlet");
+		
+		// records width and height
+		this.w = w;
+		this.h = h;
+
+		// creates a new player
+		this.player = new Player();
+	}
+
+	/*
+	 * Initializes all conponents of the game
+	 */
+	public void init(GameContainer container)
+	{
+		
+	}
+	
+	/*
+	 * Updates everything
+	 * Position, game logic, etc
+	 */
+	public void update(GameContainer container, int delta)
+	{
+		
+	}
+	
+	/*
+	Starts the game
+	*/
+	public static void main(String[] args)
+	{
+
+
+        try
+        {
+        	// sets width and height
+        	int width = 400;
+        	int height = 400;
+        	
+        	// creates the window
+            AppGameContainer app = new AppGameContainer(new Game(width, height));
+            
+            // sets the window dimensions
+            app.setDisplayMode(width, height, false);
+            
+            // starts the game
+            app.start();
+        }
+        catch (SlickException e)
+        {
+            e.printStackTrace();
+        }
+	}
+
+	/*
+	Renders components on the window
+	*/
+	public void render (GameContainer container, Graphics g) {
+
+
+	}
+
+}

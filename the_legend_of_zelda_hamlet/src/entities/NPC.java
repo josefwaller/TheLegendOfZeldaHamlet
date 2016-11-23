ab16151ecc8a9e7ceba22ed5542f43461d1dae39
josefwaller <josef@siriusapplications.com>
2016-11-23 11:42:17 -0800

    NPCs now read their dialog from file

--- the_legend_of_zelda_hamlet/src/entities/NPC.java
+++ the_legend_of_zelda_hamlet/src/entities/NPC.java
@@ -1,9 +1,16 @@
 package entities;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import entities.abstracts.InteractiveEntity;
 import game.Game;
 
 public class NPC extends InteractiveEntity {
+	
+	String dialog;
 
 	public NPC (int x, int y, String spriteSheet, String dialog, Game g) {
 		
@@ -12,6 +19,11 @@ public class NPC extends InteractiveEntity {
 		
 		// loads sprite
 		this.loadImageWithDimensions(String.format("assets/images/%s.png", spriteSheet));
+		
+		// loads dialog
+		this.dialog = Game.readFile(dialog);
+		
+		System.out.println(this.dialog);
 	}
 	
 	public void update() {

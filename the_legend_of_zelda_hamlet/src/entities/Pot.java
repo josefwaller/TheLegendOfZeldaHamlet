d9381f5ff486e0aea8034b8f67ed9684a4f08648
josefwaller <josef@siriusapplications.com>
2016-12-05 09:36:07 -0800

    Fixed Pot positioning.

--- the_legend_of_zelda_hamlet/src/entities/Pot.java
+++ the_legend_of_zelda_hamlet/src/entities/Pot.java
@@ -12,7 +12,7 @@ public class Pot extends ThrowableEntity {
 	 * Constructor
 	 */
 	public Pot(int x, int y, Game g) {
-		super(x, y, 14, g);
+		super(x + 2, y, 14, g);
 		
 		this.loadImageWithDimensions("assets/images/objects/pot.png");
 		
